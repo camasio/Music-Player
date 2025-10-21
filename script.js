54575540e938b4108fe12b250ed16de868c8d82a
camasio <magnificocamasio@gmail.com>
2025-10-21 05:41:42 +0300

    Java script inclusion to make audio music play

---
+++ script.js
@@ -0,0 +1,26 @@
+let song = document.getElementById("song");
+let progress = document.getElementById("progress");
+let control = document.getElementById("crtl-icon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+// Update progress bar as song plays
+song.ontimeupdate = function(){
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(control.classList.contains("fa-play")){
+        song.play();
+        control.classList.remove("fa-play");
+        control.classList.add("fa-pause");
+    }
+    else {
+        song.pause();
+        control.classList.remove("fa-pause");
+        control.classList.add("fa-play");
+    }
+}
